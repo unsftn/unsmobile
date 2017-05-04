8a269e7995de2bf9baa5bdd0df8fba2c5cc90847
nenadtod <nenadtod@live.com>
2017-05-04 03:27:39 +0200

    Localization done, hopefully deprecation aint a problem.

--- UNS/app/src/main/java/rs/ac/ftn/uns/MainActivity.java
+++ UNS/app/src/main/java/rs/ac/ftn/uns/MainActivity.java
@@ -1,28 +1,112 @@
 package rs.ac.ftn.uns;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
+import android.content.res.Configuration;
 import android.net.Uri;
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.widget.Button;
 import android.widget.ImageButton;
 
-import java.io.Console;
+import java.util.Locale;
+
 
 public class MainActivity extends AppCompatActivity {
 
+    private static String localization = "localization";
+    private static String language_locale_sr = "sr";
+    private static String language_locale_en = "en";
+
+    private SharedPreferences sharedPreferences;
+    private static String UNSAPP_PREFS = "unsapp_prefs";
+
+    @SuppressWarnings("deprecation")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        sharedPreferences = getSharedPreferences(UNSAPP_PREFS, MODE_PRIVATE);
+        String savedLocale = sharedPreferences.getString(localization, language_locale_sr);
+        Log.i("Mahab", savedLocale + " is the saved value");
+
+        Locale locale = new Locale(savedLocale);
+        Locale.setDefault(locale);
+        Configuration config = new Configuration();
+        config.locale = locale;
+        getBaseContext().getResources().updateConfiguration(config,
+                getBaseContext().getResources().getDisplayMetrics());
+
         setContentView(R.layout.activity_main);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+
+        initListeners();
+
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @SuppressWarnings("deprecation")
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.english_localization) {
+
+            Locale locale = new Locale(language_locale_en);
+            Locale.setDefault(locale);
+            Configuration config = new Configuration();
+            config.locale = locale;
+            getBaseContext().getResources().updateConfiguration(config,
+                    getBaseContext().getResources().getDisplayMetrics());
+
+            setContentView(R.layout.activity_main);
+            Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+            setSupportActionBar(toolbar);
+
+            SharedPreferences.Editor editor = sharedPreferences.edit();
+            editor.putString(localization, language_locale_en);
+            editor.commit();
+
+            return true;
+        }
+        if (id == R.id.serbian_localization) {
+
+            Locale locale = new Locale(language_locale_sr);
+            Locale.setDefault(locale);
+            Configuration config = new Configuration();
+            config.locale = locale;
+            getBaseContext().getResources().updateConfiguration(config,
+                    getBaseContext().getResources().getDisplayMetrics());
+
+            setContentView(R.layout.activity_main);
+            Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+            setSupportActionBar(toolbar);
+
+            SharedPreferences.Editor editor = sharedPreferences.edit();
+            editor.putString(localization, language_locale_sr);
+            editor.commit();
+
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    private void initListeners(){
 
         ImageButton buttonFaculties = (ImageButton) findViewById(R.id.facultiesButton);
         buttonFaculties.setOnClickListener(new View.OnClickListener() {
@@ -99,26 +183,6 @@ public class MainActivity extends AppCompatActivity {
             }
         });
         */
-    }
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-        return super.onOptionsItemSelected(item);
     }
 }
