5d1cba1ea3fc1a7713670b6f6310a54202dc1d89
nenadtod <nenadtod@live.com>
2017-05-09 20:28:14 +0200

    Fully implemented connection back-end!

--- UNS/app/src/main/java/rs/ac/ftn/uns/VotingActivity.java
+++ UNS/app/src/main/java/rs/ac/ftn/uns/VotingActivity.java
@@ -1,6 +1,7 @@
 package rs.ac.ftn.uns;
 
 import android.os.AsyncTask;
+import android.provider.Settings;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
@@ -20,23 +21,74 @@ public class VotingActivity extends AppCompatActivity {
         setContentView(R.layout.activity_voting);
 
 
-        FrameLayout f = (FrameLayout) findViewById(R.id.make_request);
-        f.setOnClickListener(new View.OnClickListener() {
+        FrameLayout fr = (FrameLayout) findViewById(R.id.fetch_request);
+        fr.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                String builtUri = NetworkHelper.buildUri("android");
-                new HttpFetchTask().execute(builtUri);
+
+                String android_id = Settings.Secure.getString(getBaseContext().getContentResolver(),
+                        Settings.Secure.ANDROID_ID);
+                new HttpFetchTask().execute(android_id);
+            }
+        });
+
+        FrameLayout vr = (FrameLayout) findViewById(R.id.vote_request);
+        vr.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                String android_id = Settings.Secure.getString(getBaseContext().getContentResolver(),
+                        Settings.Secure.ANDROID_ID);
+
+                new HttpPostVoteTask().execute("5911fd2b871409416cb24d4e", android_id);
+            }
+        });
+
+        FrameLayout gr = (FrameLayout) findViewById(R.id.results_request);
+        gr.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                new HttpGetVoteResultsTask().execute();
             }
         });
     }
 
     public class HttpFetchTask extends AsyncTask<String, Void, String> {
         @Override
-        protected String doInBackground(String... uris){
-            String uri = uris[0];
+        protected String doInBackground(String... ids){
+            String id = ids[0];
             String result = null;
             try{
-                result = NetworkHelper.httpGetRequest(uri);
+                result = NetworkHelper.fetchData(id);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+
+            return result;
+        }
+
+        @Override
+        protected void onPostExecute(String result){
+            if(result != null && !result.equals("")){
+                Toast.makeText(VotingActivity.this, "Pa, reklo bi se da je uspelo :)",Toast.LENGTH_SHORT).show();
+                Log.i("Mahab", result);
+            }
+        }
+
+    }
+
+    public class HttpPostVoteTask extends AsyncTask<String, Void, String> {
+        @Override
+        protected String doInBackground(String... params){
+            String candidate_id = params[0];
+            String voter_id = params[1];
+
+
+            String JSON = NetworkHelper.buildVoteJSON(candidate_id, voter_id);
+            String result = null;
+            try{
+                result = NetworkHelper.postVote(JSON);
             } catch (IOException e) {
                 e.printStackTrace();
             }
@@ -47,7 +99,31 @@ public class VotingActivity extends AppCompatActivity {
         @Override
         protected void onPostExecute(String s){
             if(s != null && !s.equals("")){
-                Toast.makeText(VotingActivity.this, "Pa, reklo bi se da je uspelo :)",Toast.LENGTH_SHORT).show();
+                Toast.makeText(VotingActivity.this, "Pa, reklo bi se da si glasao:)",Toast.LENGTH_SHORT).show();
+                Log.i("Mahab", s);
+            }
+        }
+
+    }
+
+    public class HttpGetVoteResultsTask extends AsyncTask<Void, Void, String> {
+        @Override
+        protected String doInBackground(Void... params){
+
+            String result = "";
+            try{
+                result = NetworkHelper.getVotesResult();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+
+            return result;
+        }
+
+        @Override
+        protected void onPostExecute(String s){
+            if(s != null && !s.equals("")){
+                Toast.makeText(VotingActivity.this, "Pa, reklo bi se da si glasao:)",Toast.LENGTH_SHORT).show();
                 Log.i("Mahab", s);
             }
         }
